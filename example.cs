287397f3a738e5853b841c2d32b975f85292f4e1
12ureiN <lsxchungz@gmail.com>
2017-04-19 22:41:40 +1000

    Adding age to Example Class

--- example.cs
+++ example.cs
@@ -3,10 +3,12 @@ https://github.com/Nieru21/Conflict.git
 public class Example
 {
 	private String name;
+	private int age;
 
-	public Example(String Name)
+	public Example(String Name, int age)
 	{
 		this.name = Name;
+		this.age  = age ;
 	}
 
 }
